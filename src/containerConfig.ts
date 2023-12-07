30e91a09e8b4c2d049e49d7db35a591b3b694cbc
heliprokupetz14 <139674916+heliprokupetz14@users.noreply.github.com>
2023-12-07 10:38:01 +0200

    Initialize upgrade_packages

---
+++ src/containerConfig.ts
@@ -0,0 +1,35 @@
+import config from 'config';
+import { getOtelMixin } from '@map-colonies/telemetry';
+import { trace, metrics as OtelMetrics } from '@opentelemetry/api';
+import { DependencyContainer } from 'tsyringe/dist/typings/types';
+import jsLogger, { LoggerOptions } from '@map-colonies/js-logger';
+import { Metrics } from '@map-colonies/telemetry';
+import { SERVICES, SERVICE_NAME } from './common/constants';
+import { tracing } from './common/tracing';
+import { InjectionObject, registerDependencies } from './common/dependencyRegistration';
+
+export interface RegisterOptions {
+  override?: InjectionObject<unknown>[];
+  useChild?: boolean;
+}
+
+export const registerExternalValues = (options?: RegisterOptions): DependencyContainer => {
+  const loggerConfig = config.get<LoggerOptions>('telemetry.logger');
+  const logger = jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, mixin: getOtelMixin() });
+
+  const metrics = new Metrics();
+  metrics.start();
+
+  tracing.start();
+  const tracer = trace.getTracer(SERVICE_NAME);
+
+  const dependencies: InjectionObject<unknown>[] = [
+    { token: SERVICES.CONFIG, provider: { useValue: config } },
+    { token: SERVICES.LOGGER, provider: { useValue: logger } },
+    { token: SERVICES.TRACER, provider: { useValue: tracer } },
+    { token: SERVICES.METER, provider: { useValue: OtelMetrics.getMeterProvider().getMeter(SERVICE_NAME) } },
+    { token: SERVICES.METRICS, provider: { useValue: metrics } },
+  ];
+
+  return registerDependencies(dependencies, options?.override, options?.useChild);
+};
